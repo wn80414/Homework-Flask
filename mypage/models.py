2ad2abc633eb1fa50527c517e1ee97c98681f976
William Nguyen <william.g.nguyen@sjsu.edu>
2021-11-08 13:58:21 -0800

    Changed __repr__ function to only print city names

--- mypage/models.py
+++ mypage/models.py
@@ -4,4 +4,4 @@ class City(db.Model):
     city_name = db.Column(db.String(80), unique=True, nullable=False)
     city_rank = db.Column(db.Integer, primary_key=True)
     def __repr__(self):
-        return f'<{self.city_name}: {self.city_rank}>'
+        return f'{self.city_name}'
